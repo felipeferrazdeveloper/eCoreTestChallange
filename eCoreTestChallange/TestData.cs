e41aeba2285474a7d0338017208b0d7215b976be
felipe <felipe.cintra@fdte.io>
2023-05-24 10:56:41 -0300

    Adding dataTable to testcases

---
+++ eCoreTestChallange/TestData.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCoreTestChallange
+{
+    public static class TestData
+    {
+        public static DataTable testData;       
+
+        public static DataTable PositiveLogin()
+        {
+            testData = new DataTable();
+
+            testData.Columns.Add("username", typeof(string));
+            testData.Columns.Add("password", typeof(string));
+
+            testData.Rows.Add("demouser", "abc123");
+            return testData;
+        }        
+    }
+}
--- eCoreTestChallange/Tests.cs
+++ eCoreTestChallange/Tests.cs
@@ -1,5 +1,8 @@
 using eCoreTestChallange.PageObjects.Page;
 using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using System.Collections;
+using System.Data;
 using DescriptionAttribute = NUnit.Framework.DescriptionAttribute;
 
 namespace eCoreTestChallange
@@ -8,29 +11,30 @@ namespace eCoreTestChallange
     public class Tests : TestBase
     {
         [Test]
-        [Description("TC001 User can authenticate in the application with provided credentials")]
-        public void PositiveLogin()
-        {
+        [Description("TC001 User can authenticate in the application with valid credentials")]
+        [TestCaseSource(nameof(PositiveLoginDataSet))]
+        public void PositiveLogin(String username, String password)
+        {                       
             LoginPage loginPage = new();
             InvoiceListPage invoiceListPage = new();
             
             _ = loginPage
-                .LoginWithCredentials("demouser", "abc123");
+                .LoginWithCredentials(username, password);
            
             _ = invoiceListPage
                 .AssertUserIsOnPage();                   
         }
 
-        [Test]        
-        public void NegativeLogin() 
-        {
-           
-        }
-        
         [Test]
-        public void ValidateInvoiceDetails() 
+        public void NegativeLogin()
         {
-           
+
+        }
+
+        [Test]
+        public void ValidateInvoiceDetails()
+        {
+
         }
     }
 }
